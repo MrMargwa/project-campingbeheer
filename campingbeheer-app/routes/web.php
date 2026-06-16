aea1eaf195f467e5e4f0a0713f988b985a675951
MrMargwa <justin_haringa@outlook.com>
2026-06-16 10:41:18 +0200

    Reserveren controller gefixed

--- campingbeheer-app/routes/web.php
+++ campingbeheer-app/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\BoekingController;
-use App\Http\Controllers\ReserverenController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\BookingController;
 use App\Models\Accommodatie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
